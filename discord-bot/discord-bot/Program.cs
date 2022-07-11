1ee0e50233cd1a13d7406dbbd9e2f4d29ba110a4
Viktor Soukup <vitosoukup@gmail.com>
2022-07-11 16:52:19 +0200

    Remove the bot token
    (The token will not work, it has been reset.)

--- discord-bot/discord-bot/Program.cs
+++ discord-bot/discord-bot/Program.cs
@@ -8,8 +8,7 @@ namespace bot
 
 		public async Task MainAsync()
 		{
-			var bot = new Bot("3011", "NTQwNjYxOTIxNTQ0OTI5Mjk4.XFN4pw.1yTaeEwl2GIuCHSl2aHgDo5g3Rg");
-			//var bot = new Bot("3011", "Insert your token here.");
+			var bot = new Bot("3011", "Insert your token here.");
 
 			bot.AddCommand(new HelpCommand(bot));
 			bot.AddModule(ReplyModule.MakeModule("reply"));
